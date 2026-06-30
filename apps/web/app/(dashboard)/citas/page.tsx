fca29f340adeb2543c5c9ad633a8ffb231262485
Alan79238 <mendezalan045@gmail.com>
2026-06-29 19:20:46 -0600

    Push de alan
    Alan push

--- apps/web/app/(dashboard)/citas/page.tsx
+++ apps/web/app/(dashboard)/citas/page.tsx
@@ -44,7 +44,7 @@ const seedData: Appointment[] = [
   },
 ];
 
-//Holaaaa
+//Holaaaaaaaa alan
 function toMinutes(value: string) {
   const [h, m] = value.split(":").map(Number);
   return h * 60 + m;
